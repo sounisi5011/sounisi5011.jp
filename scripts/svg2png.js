8b82cbae8c3b996b4078ed0ac55245e159e78b9a
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-07 05:03:29 +0900

    書き込み先のディレクトリが存在しない場合に、ディレクトリを自動で生成するように修正
    いくつかのnpm scriptsで、書き込み先のディレクトリが存在しない場合も正常に処理するように修正した。

--- scripts/svg2png.js
+++ scripts/svg2png.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 const { convertFile } = require('convert-svg-to-png');
+const makeDir = require('make-dir');
 const path = require('path');
 
 async function main() {
   const sourceFileFullpath = path.resolve(process.argv[2]);
   const destFileFullpath = path.resolve(process.argv[3]);
+
+  await makeDir(path.dirname(destFileFullpath));
 
   await convertFile(sourceFileFullpath, {
     outputFilePath: destFileFullpath,
