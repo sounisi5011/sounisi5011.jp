1aff7db3b023eb5719bca47d4d4abbad53a32131
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-08 06:29:32 +0900

    テンプレートで使用するページのパス用変数に、Pugを変換した後のhtmlパスを指定するように変更

--- index.js
+++ index.js
@@ -20,11 +20,18 @@ Metalsmith(__dirname)
     const metadata = metalsmith.metadata();
     const rootURL = metadata.url;
 
-    Object.entries(files).forEach(([filepath, filedata]) => {
-      filedata.rootURL = rootURL;
-      filedata.canonicalURL =
-        rootURL.replace(/\/*$/, '') + filepath.replace(/^\/*/, '/');
-    });
+    Object.entries(files)
+      .map(([filepath, filedata]) => [
+        filepath
+          .replace(/\.pug$/, '.html')
+          .replace(/(?:^|\/)index\.html?$/, ''),
+        filedata,
+      ])
+      .forEach(([filepath, filedata]) => {
+        filedata.rootURL = rootURL;
+        filedata.canonicalURL =
+          rootURL.replace(/\/*$/, '') + filepath.replace(/^\/*/, '/');
+      });
 
     done();
   })
