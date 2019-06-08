90602039816f8795caf4c4458e893c40f9a7e46a
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-09 05:15:00 +0900

    Netlifyのデータをメタデータに定義するMetalsmith用のローカル・プラグインを作成

---
+++ src/plugins/netlifyMetadata.js
@@ -0,0 +1,21 @@
+const DEPLOY_URL_REGEXP = /^https?:\/\/[0-9a-fA-F]+--([0-9a-zA-Z-]+\.netlify\.com)/;
+const DOMAIN_REGEXP = /^https?:\/\/([0-9a-zA-Z.-]+)/;
+
+module.exports = () => {
+  return (files, metalsmith, done) => {
+    const env = process.env;
+    const metadata = metalsmith.metadata();
+
+    const deployUrlMatch = DEPLOY_URL_REGEXP.exec(env.DEPLOY_URL);
+    if (deployUrlMatch) {
+      metadata.netlifyDefaultSubdomain = deployUrlMatch[1];
+    }
+
+    const domainMatch = DOMAIN_REGEXP.exec(env.URL);
+    if (domainMatch) {
+      metadata.netlifyPrimaryDomain = domainMatch[1];
+    }
+
+    done();
+  };
+};
