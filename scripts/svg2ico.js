7d7c59d51723c58a6e23690d41831346606c4599
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-09 03:38:45 +0900

    新しいESLintのルールに適合するように、既存のファイルを修正

--- scripts/svg2ico.js
+++ scripts/svg2ico.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const util = require('util');
 
 const { convert } = require('convert-svg-to-png');
