24f6cf269fa618374b5614f7627cc2eb0a74f6aa
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-09 23:06:22 +0900

    メタデータプロパティ pageStyles を globalPageStyles に変更

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ Metalsmith(__dirname)
   .metadata({
     description: 'sounisi5011の創作とソーシャルサービスの集約サイト',
     generator: 'Metalsmith',
-    pageStyles: ['/default.css'],
+    globalPageStyles: ['/default.css'],
     title: 'sounisi5011.jp',
     url:
       (process.env.CONTEXT === 'production'
