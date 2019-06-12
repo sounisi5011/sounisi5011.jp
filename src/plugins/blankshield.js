2486da998c545d50c3b1bdf3cead89fd174eb56c
Sonishi Izuka <sounisi5011@jedofuka.local>
2019-06-13 01:08:38 +0900

    Tabnabbingを修正するローカル・プラグインを作成

--- src/plugins/blankshield.js
+++ src/plugins/blankshield.js
@@ -1,19 +1,14 @@
-const htmlparser = require('htmlparser2');
-const pluginKit = require('metalsmith-plugin-kit');
+const path = require('path');
 
-const parser = new htmlparser.Parser(
-  {
-    onopentag: function(name, attribs) {
-      if (name === 'a') {
-        console.log({ attribs });
-      }
-    },
-  },
-  { decodeEntities: true },
+const debug = require('debug')(
+  `metalsmith-${path.relative(process.cwd(), __filename)}`,
 );
+const cheerio = require('cheerio');
+const pluginKit = require('metalsmith-plugin-kit');
 
 module.exports = opts => {
   const options = {
+    insertNoreferrer: false,
     pattern: ['**/*.html', '**/*.htm'],
     ...opts,
   };
@@ -21,9 +16,46 @@ module.exports = opts => {
   return pluginKit.middleware({
     each: async (filename, file, files, metalsmith) => {
       const htmlData = file.contents.toString();
-      parser(htmlData);
-      const fixedHtmlData = htmlData;
-      file.contents = Buffer.from(fixedHtmlData);
+      const $ = cheerio.load(htmlData);
+      let isUpdated = false;
+
+      $('a[target=_blank]').each((i, elem) => {
+        const $elem = $(elem);
+        const relValue = $elem.attr('rel');
+        const newRelList = [];
+
+        if (!$elem.is('[rel~=noopener]')) {
+          newRelList.push('noopener');
+        }
+
+        if (options.insertNoreferrer && !$elem.is('[rel~=noreferrer]')) {
+          newRelList.push('noreferrer');
+        }
+
+        if (newRelList.length > 0) {
+          const newRelValue =
+            (relValue ? `${relValue} ` : '') + newRelList.join(' ');
+
+          $elem.attr('rel', newRelValue);
+          if (typeof relValue === 'string') {
+            debug(
+              '%s: <a rel="%s"> -> <a rel="%s">',
+              filename,
+              relValue,
+              newRelValue,
+            );
+          } else {
+            debug('%s: <a> -> <a rel="%s">', filename, newRelValue);
+          }
+
+          isUpdated = true;
+        }
+      });
+
+      if (isUpdated) {
+        const fixedHtmlData = $.html();
+        file.contents = Buffer.from(fixedHtmlData);
+      }
     },
     match: options.pattern,
   });
