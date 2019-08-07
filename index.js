323b226a073db17fcc6e032d74bc31b6cef4903b
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-07 14:40:59 +0900

    絶対URLを含む要素の属性も置換するように修正

--- index.js
+++ index.js
@@ -106,23 +106,55 @@ Metalsmith(__dirname)
          * @see https://github.com/sounisi5011/metalsmith-netlify-published-date/blob/v0.1.0/example/remove-time-elem.js
          */
 
+        const netlifyDeployUrlRegExp = /^https?:\/\/[0-9a-f]+--[0-9a-z-]+\.netlify\.com(?=[/?#]|$)/i;
+        let isUpdated = false;
+
         try {
           const $ = cheerio.load(contents.toString());
 
-          const $timeElems = $(
-            'time[itemprop~=datePublished],time[itemprop~=dateModified]',
+          // canonical URLを置換
+          $('head link[rel~=canonical][href^="http"]').each(
+            (index, element) => {
+              const $meta = $(element);
+              const hrefAttr = $meta.attr('href');
+              if (netlifyDeployUrlRegExp.test(hrefAttr)) {
+                $meta.attr(
+                  'href',
+                  hrefAttr.replace(netlifyDeployUrlRegExp, process.env.URL),
+                );
+                isUpdated = true;
+              }
+            },
           );
 
-          // Note: If the file contents is not valid HTML, cheerio will not throw an error.
-          //       However, the number of detected "time" elements will be 0.
-          if ($timeElems.length >= 1) {
-            $timeElems.each((index, element) => {
+          // OGPの絶対URLを置換
+          $(
+            'head meta:matches([property^="og:"], [property^="twitter:"])[content^="http"]',
+          ).each((index, element) => {
+            const $meta = $(element);
+            const contentAttr = $meta.attr('content');
+            if (netlifyDeployUrlRegExp.test(contentAttr)) {
+              $meta.attr(
+                'content',
+                contentAttr.replace(netlifyDeployUrlRegExp, process.env.URL),
+              );
+              isUpdated = true;
+            }
+          });
+
+          // itemprop属性を持つtime要素を置換
+          $('time[itemprop~=datePublished],time[itemprop~=dateModified]').each(
+            (index, element) => {
               const $time = $(element);
               $time.empty();
               if ($time.is('[datetime]')) {
                 $time.attr('datetime', '');
               }
-            });
+              isUpdated = true;
+            },
+          );
+
+          if (isUpdated) {
             return Buffer.from($.html());
           }
         } catch (err) {
