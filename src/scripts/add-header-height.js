816aa4f32b851a3f811113b312fd78e2484f2e62
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-18 15:55:26 +0900

    ヘッダの高さを、resizeイベントの発生時にも更新するように修正

--- src/scripts/add-header-height.js
+++ src/scripts/add-header-height.js
@@ -1,4 +1,16 @@
 (function(window, document, CSS) {
+  var requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+  if (typeof requestAnimationFrame !== 'function') {
+    requestAnimationFrame = function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+  }
+
   if (typeof CSS.supports !== 'function') {
     return;
   }
@@ -13,8 +25,23 @@
     CSS.supports('position', '-webkit-sticky')
   ) {
     var headerElem = document.querySelector('header.page');
-    var headerHeight = headerElem.getBoundingClientRect().height;
+    var updateProp = function() {
+      var headerHeight = headerElem.getBoundingClientRect().height;
+      rootStyle.setProperty('--header-height', `${headerHeight}px`);
+    };
 
-    rootStyle.setProperty('--header-height', `${headerHeight}px`);
+    var updating = false;
+    var resizeListener = function() {
+      if (!updating) {
+        updating = true;
+        requestAnimationFrame(function() {
+          updateProp();
+          updating = false;
+        });
+      }
+    };
+
+    window.addEventListener('resize', resizeListener, false);
+    updateProp();
   }
 })(window, document, window.CSS || {});
