b0877efc93c4412ee2a0fa7e0224e5dc6ac26b5e
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-18 17:15:43 +0900

    src/scriptsディレクトリ内のスクリプトをES2015に書き直し

--- src/scripts/add-header-height.js
+++ src/scripts/add-header-height.js
@@ -1,21 +1,16 @@
-(function(window, document, CSS) {
-  var requestAnimationFrame =
-    window.requestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame;
-  if (typeof requestAnimationFrame !== 'function') {
-    requestAnimationFrame = function(callback) {
-      window.setTimeout(callback, 1000 / 60);
-    };
-  }
+((window, document, CSS) => {
+  const requestAnimationFrame =
+    typeof window.requestAnimationFrame === 'function'
+      ? window.requestAnimationFrame
+      : callback => {
+          setTimeout(callback, 0);
+        };
 
   if (typeof CSS.supports !== 'function') {
     return;
   }
 
-  var rootStyle = document.documentElement.style;
+  const rootStyle = document.documentElement.style;
   if (typeof rootStyle.setProperty !== 'function') {
     return;
   }
@@ -24,17 +19,17 @@
     CSS.supports('position', 'sticky') ||
     CSS.supports('position', '-webkit-sticky')
   ) {
-    var headerElem = document.querySelector('header.page');
-    var updateProp = function() {
-      var headerHeight = headerElem.getBoundingClientRect().height;
+    const headerElem = document.querySelector('header.page');
+    const updateProp = () => {
+      const headerHeight = headerElem.getBoundingClientRect().height;
       rootStyle.setProperty('--header-height', `${headerHeight}px`);
     };
 
-    var updating = false;
-    var resizeListener = function() {
+    let updating = false;
+    const resizeListener = () => {
       if (!updating) {
         updating = true;
-        requestAnimationFrame(function() {
+        requestAnimationFrame(() => {
           updateProp();
           updating = false;
         });
