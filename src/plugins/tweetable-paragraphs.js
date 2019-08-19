ab859981fcc523bdc82ee5c1ef51e95391cee9b1
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-19 18:30:44 +0900

    HTMLの内容を文字列化する処理で、p要素やpre要素等のデフォルトスタイルを考慮するように修正
    * p要素等で囲まれたテキストの前後には空行を挿入する。
    * テキストの空白文字は半角スペースに置換する。ただし、pre要素の内容は除く。

--- src/plugins/tweetable-paragraphs.js
+++ src/plugins/tweetable-paragraphs.js
@@ -7,6 +7,12 @@ const debug = require('debug')(
 const cheerio = require('cheerio');
 const multimatch = require('multimatch');
 
+/**
+ * @see https://infra.spec.whatwg.org/#ascii-whitespace
+ * @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes
+ */
+const HTML_WS_REGEXP = /[\t\n\f\r ]+/g;
+
 function last(list) {
   return list[list.length - 1];
 }
@@ -16,7 +22,12 @@ function createData($, idNode, text) {
   return {
     id: id !== undefined ? id : null,
     idNode,
-    text,
+    margin: {
+      bottom: -1,
+      top: -1,
+    },
+    rawText: text,
+    text: text.replace(HTML_WS_REGEXP, ' '),
   };
 }
 
@@ -40,37 +51,79 @@ function readTextContents($, elem, opts = {}, prevIdNode = null) {
       dataList.push(createData($, prevIdNode, node.data));
     } else if (node.type === 'tag' && $node.is(ignoreElemSelector)) {
       let currentIdNode = $node.is('[id]') ? node : prevIdNode;
-      dataList.push(
-        ...$node
-          .contents()
-          .get()
-          .reduce(
-            (list, childNode) => {
-              const data = readTextContents(
-                $,
-                $(childNode),
-                options,
-                currentIdNode,
-              );
-
-              if (last(data)) {
-                currentIdNode = last(data).idNode;
-              }
-
-              const firstDataItem = data[0];
-              const lastListItem = last(list);
-              if (firstDataItem && lastListItem) {
-                if (lastListItem.idNode === firstDataItem.idNode) {
-                  lastListItem.text += firstDataItem.text;
-                  data.shift();
-                }
-              }
-
-              return [...list, ...data];
-            },
-            currentIdNode !== null ? [createData($, currentIdNode, '')] : [],
-          ),
+      /**
+       * @see https://chromium.googlesource.com/chromium/blink/+/master/Source/core/css/html.css
+       * @see https://dxr.mozilla.org/mozilla-central/source/layout/style/res/html.css
+       * @see http://trac.webkit.org/browser/trunk/Source/WebCore/css/html.css
+       */
+      const isPreElem = $node.is('pre, xmp, plaintext, listing, textarea');
+      const isPElem = $node.is(
+        'p, blockquote, figure, h1, h2, h3, h4, h5, h6, hr, ul, menu, dir, ol, dl, multicol, pre, xmp, plaintext, listing',
       );
+
+      const newDataList = $node
+        .contents()
+        .get()
+        .reduce(
+          (list, childNode) => {
+            const data = readTextContents(
+              $,
+              $(childNode),
+              options,
+              currentIdNode,
+            );
+
+            /*
+             * pre要素の内容のスペース文字は保持する
+             */
+            if (isPreElem) {
+              data.forEach(data => {
+                data.text = data.rawText;
+              });
+            }
+
+            if (last(data)) {
+              currentIdNode = last(data).idNode;
+            }
+
+            const firstDataItem = data[0];
+            const lastListItem = last(list);
+            if (firstDataItem && lastListItem) {
+              if (lastListItem.idNode === firstDataItem.idNode) {
+                /*
+                 * 前後のマージンの数だけ空行を生成する
+                 */
+                const marginLinesNumber = Math.max(
+                  lastListItem.margin.bottom,
+                  firstDataItem.margin.top,
+                );
+                const marginLines = '\n'.repeat(
+                  Math.max(0, 1 + marginLinesNumber),
+                );
+
+                lastListItem.rawText += marginLines + firstDataItem.rawText;
+                lastListItem.text += marginLines + firstDataItem.text;
+                data.shift();
+              }
+            }
+
+            return [...list, ...data];
+          },
+          currentIdNode !== null ? [createData($, currentIdNode, '')] : [],
+        );
+
+      /*
+       * p要素の前後には、一行のマージンを追加する
+       */
+      if (isPElem) {
+        const margin = 1;
+        const firstData = newDataList[0];
+        const lastData = last(newDataList);
+        firstData.margin.top = Math.max(margin, firstData.margin.top);
+        lastData.margin.bottom = Math.max(margin, lastData.margin.bottom);
+      }
+
+      dataList.push(...newDataList);
     }
   });
 
