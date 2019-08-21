77b1719cb77afabd28fdf0ed4ae9c4016da32501
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-21 23:31:54 +0900

    共有ボタンの開閉機能と、ページの共有とページ内共有を切り替えるボタンを追加

--- src/scripts/paragraphs-share.js
+++ src/scripts/paragraphs-share.js
@@ -53,6 +53,27 @@
     return linkElem.href || linkElem.getAttribute('href');
   }
 
+  function prependClass(elem, className) {
+    elem.className = className + ' ' + elem.className;
+    return elem;
+  }
+
+  function createBottomFixedBoxElem(childrenCallback) {
+    const outerElem = document.createElement('div');
+    outerElem.className = 'bottom-fixed-box outer';
+
+    const innerElem = document.createElement('div');
+    innerElem.className = 'bottom-fixed-box inner';
+    outerElem.appendChild(innerElem);
+
+    const childrenList = childrenCallback(innerElem);
+    for (let i = 0; i < childrenList.length; i++) {
+      innerElem.appendChild(childrenList[i]);
+    }
+
+    return outerElem;
+  }
+
   function share({ title = document.title, text, url }) {
     if (typeof navigator.share === 'function') {
       navigator.share({ text, title, url });
@@ -62,44 +83,84 @@
   }
 
   const canonicalURL = getCanonicalURL() || location.href.replace(/#.*$/, '');
+  const htmlElem = document.documentElement;
   const headerElem = document.querySelector('header.page');
   const footerElem = document.querySelector('footer.page');
 
-  const shareAreaElem = document.createElement('div');
-  shareAreaElem.className = 'share-area';
+  const shareAreaElem = createBottomFixedBoxElem(shareInnerAreaElem => {
+    const shareButtonElem = document.createElement('button');
+    shareButtonElem.className = 'share-button toggle-menu';
+    shareButtonElem.textContent = '共有';
+    shareButtonElem.addEventListener(
+      'click',
+      () => {
+        if (htmlElem.classList.contains('share-open')) {
+          htmlElem.classList.remove('share-open');
+          shareButtonElem.textContent = '共有';
+        } else {
+          htmlElem.classList.add('share-open');
+          shareButtonElem.textContent = '閉じる';
+        }
 
-  const shareButtonElem = document.createElement('button');
-  shareButtonElem.className = 'share-button';
-  shareButtonElem.textContent = '共有';
-  shareButtonElem.addEventListener(
-    'click',
-    () => {
-      const currentParagraphElem = findElem(
-        document.querySelectorAll('main.novel-body>*'),
-        elem => {
-          // see https://blog.jxck.io/entries/2016-06-25/intersection-observer.html#表示判定
-          return isIntersection(elem, {
-            padding: {
-              bottom: footerElem.getBoundingClientRect().height,
-              top: headerElem.getBoundingClientRect().height,
-            },
-          });
-        },
-      );
-      const currentParagraphIDElem = getPrevIDElem(currentParagraphElem);
+        const currentParagraphElem = findElem(
+          document.querySelectorAll('main.novel-body>*'),
+          elem => {
+            // see https://blog.jxck.io/entries/2016-06-25/intersection-observer.html#表示判定
+            return isIntersection(elem, {
+              padding: {
+                bottom: footerElem.getBoundingClientRect().height,
+                top: headerElem.getBoundingClientRect().height,
+              },
+            });
+          },
+        );
+        const currentParagraphIDElem = getPrevIDElem(currentParagraphElem);
 
-      const fragment = currentParagraphIDElem
-        ? '#' + encodeURIComponent(currentParagraphIDElem.id)
-        : '';
-      const url = canonicalURL + fragment;
-      share({
-        text: currentParagraphIDElem.getAttribute('data-share-text'),
-        url,
-      });
-    },
-    false,
-  );
-  shareAreaElem.appendChild(shareButtonElem);
+        const fragment = currentParagraphIDElem
+          ? '#' + encodeURIComponent(currentParagraphIDElem.id)
+          : '';
+        const url = canonicalURL + fragment;
+        share({
+          text: currentParagraphIDElem.getAttribute('data-share-text'),
+          url,
+        });
+      },
+      false,
+    );
 
+    const paragraphShareAreaElem = createBottomFixedBoxElem(
+      paragraphShareInnerAreaElem => {
+        const paragraphShareButtonElem = document.createElement('button');
+        paragraphShareButtonElem.className = 'share-button paragraph-share';
+        paragraphShareButtonElem.textContent = '一部を共有';
+        paragraphShareButtonElem.addEventListener(
+          'click',
+          () => {
+            htmlElem.classList.add('paragraph-share');
+            allShareButtonElem.focus();
+          },
+          false,
+        );
+
+        const allShareButtonElem = document.createElement('button');
+        allShareButtonElem.className = 'share-button all-share';
+        allShareButtonElem.textContent = '全体を共有';
+        allShareButtonElem.addEventListener(
+          'click',
+          () => {
+            htmlElem.classList.remove('paragraph-share');
+            paragraphShareButtonElem.focus();
+          },
+          false,
+        );
+
+        return [paragraphShareButtonElem, allShareButtonElem];
+      },
+    );
+    prependClass(paragraphShareAreaElem, 'paragraph-share-area');
+
+    return [shareButtonElem, paragraphShareAreaElem];
+  });
+  prependClass(shareAreaElem, 'share-area');
   footerElem.insertBefore(shareAreaElem, footerElem.firstChild);
 }
