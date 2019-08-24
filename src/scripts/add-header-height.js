5d13ccf213fb4296d502024e8cf38158e734d1e4
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-25 00:35:12 +0900

    クエリパラメータ fragment が存在する場合にページのURLをリライトしてスクロールするスクリプトを追加

--- src/scripts/add-header-height.js
+++ src/scripts/add-header-height.js
@@ -1,46 +1,88 @@
 (window => {
-  const { document } = window;
+  const { document, location, history } = window;
   const CSS = window.CSS || {};
   const rootStyle = document.documentElement.style;
   const isFunc = value => typeof value === 'function';
 
-  if (!isFunc(CSS.supports)) {
-    return;
-  }
+  /**
+   * @see https://html.spec.whatwg.org/multipage/browsing-the-web.html#scroll-to-the-fragment-identifier
+   */
+  const scrollToFragment = fragment => {
+    if (fragment !== '') {
+      let potentialIndicatedElement = document.getElementById(fragment);
 
-  if (!isFunc(rootStyle.setProperty)) {
-    return;
-  }
+      if (!potentialIndicatedElement) {
+        const decodedFragment = decodeURIComponent(fragment);
+        potentialIndicatedElement = document.getElementById(decodedFragment);
+        if (!potentialIndicatedElement) {
+          if (!/^top$/i.test(decodedFragment)) {
+            return;
+          }
+        }
+      }
 
-  if (
-    !(
+      if (potentialIndicatedElement) {
+        potentialIndicatedElement.scrollIntoView();
+        return;
+      }
+    }
+    window.scrollTo(0, 0);
+  };
+
+  if (isFunc(CSS.supports) && isFunc(rootStyle.setProperty)) {
+    if (
       CSS.supports('position', 'sticky') ||
       CSS.supports('position', '-webkit-sticky')
-    )
-  ) {
-    return;
+    ) {
+      const headerElem = document.querySelector('header.page');
+      const updateProp = () => {
+        const headerHeight = headerElem.getBoundingClientRect().height;
+        rootStyle.setProperty('--header-height', headerHeight + 'px');
+      };
+
+      let updating = false;
+      const resizeListener = () => {
+        if (!updating) {
+          updating = true;
+          // Note: CSS.supports()とposition:stickyに対応しているブラウザは、
+          //       全てrequestAnimationFrame()に対応している。
+          //       polyfillは不要
+          window.requestAnimationFrame(() => {
+            updateProp();
+            updating = false;
+          });
+        }
+      };
+
+      window.addEventListener('resize', resizeListener, false);
+      updateProp();
+    }
   }
 
-  const headerElem = document.querySelector('header.page');
-  const updateProp = () => {
-    const headerHeight = headerElem.getBoundingClientRect().height;
-    rootStyle.setProperty('--header-height', headerHeight + 'px');
-  };
+  /*
+   * ページのURLにクエリパラメータ fragment が存在する場合は、URLをリライトする
+   */
+  const origURL = location.href;
+  let fragmentID = false;
+  const queryRemovedURL = origURL.replace(
+    /(^|[?&])fragment=([^&#]*)(&|(?=#)|$)/g,
+    (_, beforeSep, id, afterSep) => {
+      if (fragmentID === false) {
+        fragmentID = id;
+      }
+      return afterSep ? beforeSep : '';
+    },
+  );
+  if (origURL !== queryRemovedURL) {
+    const newURL = queryRemovedURL.replace(/(?:#.*)?$/, '#' + fragmentID);
 
-  let updating = false;
-  const resizeListener = () => {
-    if (!updating) {
-      updating = true;
-      // Note: CSS.supports()とposition:stickyに対応しているブラウザは、
-      //       全てrequestAnimationFrame()に対応している。
-      //       polyfillは不要
-      window.requestAnimationFrame(() => {
-        updateProp();
-        updating = false;
-      });
+    if (history && isFunc(history.replaceState)) {
+      history.replaceState(null, '', newURL);
+      scrollToFragment(fragmentID);
+    } else if (isFunc(location.replace)) {
+      location.replace(newURL);
+    } else {
+      location.href = newURL;
     }
-  };
-
-  window.addEventListener('resize', resizeListener, false);
-  updateProp();
+  }
 })(window);
