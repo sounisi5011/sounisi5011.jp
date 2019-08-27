050f79c5c6646883067efa0cab31e059efdc8d75
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-28 08:14:00 +0900

    クエリストリングのfragmentパラメータを元に、リライトとリダイレクトを行うNetlify Functionスクリプトを追加

---
+++ src/lambda/fragment-rewrite/fragment-rewrite.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+const dataDirname = 'assets';
+
+exports.handler = async (event, context) => {
+  const fragment = event.queryStringParameters.fragment;
+  if (fragment) {
+    const filepathList = [
+      `${dataDirname}/${fragment}/${event.path}.html`,
+      `${dataDirname}/${fragment}/${event.path}/index.html`,
+    ];
+
+    for (const filepath of filepathList) {
+      try {
+        const body = await readFile(require.resolve(`./${filepath}`));
+        return {
+          body,
+          headers: {
+            'content-type': 'text/html; charset=utf-8',
+          },
+          statusCode: 200,
+        };
+      } catch (err) {
+        //
+      }
+    }
+
+    return {
+      headers: {
+        location: event.path + `#${encodeURIComponent(fragment)}`,
+      },
+      statusCode: 301,
+    };
+  }
+
+  return {
+    headers: {
+      location: event.path,
+    },
+    statusCode: 301,
+  };
+};
