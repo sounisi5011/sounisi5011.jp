fa7b32553f4e891e94257a6beec94423809a40c4
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-28 07:12:03 +0900

    ファイルシステムのエラーを回避する

--- src/lambda/test.js
+++ src/lambda/test.js
@@ -3,14 +3,20 @@ const util = require('util');
 
 const readdir = util.promisify(fs.readdir);
 
+const pathList = ['.', '..'];
+
 exports.handler = async (event, context) => {
-  console.log({ context, event });
+  const dir = {};
+  for (const path of pathList) {
+    try {
+      dir[path] = await readdir(path);
+    } catch (err) {
+      dir[path] = err.message;
+    }
+  }
+
   return {
-    body: JSON.stringify(
-      { context, dir: await readdir('../_fragment-anchors/'), event },
-      null,
-      2,
-    ),
+    body: JSON.stringify({ context, dir, event }, null, 2),
     statusCode: 200,
   };
 };
