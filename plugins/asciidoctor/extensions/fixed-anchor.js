8690ed70f35db6da1ff050119f6095d94972a1b8
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2020-02-20 17:38:50 +0900

    anchorマクロを再定義するAsciidoctorの拡張を、backendの処理にフォールバックするように修正
    独自にHTML文字列を生成せず、Asciidoctorのメソッドを使用して生成する。これにより、asciidoctor-html5sパッケージのクールでアクセシビリティに優れたHTML定義を採用することができる。

--- plugins/asciidoctor/extensions/fixed-anchor.js
+++ plugins/asciidoctor/extensions/fixed-anchor.js
@@ -1,11 +1,12 @@
 /**
+ * anchorをマクロとして再定義する。これにより、全角のコロンを含むid属性も処理できるようになる。また、不正なid属性値をエラー報告することが可能になる。
  * @see https://asciidoctor.org/docs/user-manual/#anchordef
  * @see https://asciidoctor-docs.netlify.com/asciidoctor.js/extend/extensions/inline-macro-processor/
+ * @see https://github.com/jirutka/asciidoctor-html5s/blob/v0.2.0/data/templates/inline_anchor.html.slim
+ * @see https://asciidoctor.github.io/asciidoctor.js/master/#extensionsprocessor
  */
 
 const util = require('util');
-
-const { wrapAttrValue } = require('./utils/html');
 
 /**
  * @see https://infra.spec.whatwg.org/#ascii-whitespace
@@ -15,7 +16,7 @@ const HTML_WS_REGEXP = /[\t\n\f\r ]+/g;
 
 module.exports = registry => {
   registry.inlineMacro('anchor', function() {
-    this.process((_, id) => {
+    this.process((parent, id) => {
       /** @see https://momdo.github.io/html/dom.html#the-id-attribute */
       if (id === '') throw new Error('id属性は空文字列を許可していません');
       if (HTML_WS_REGEXP.test(id))
@@ -25,7 +26,7 @@ module.exports = registry => {
           )}`,
         );
 
-      return `<a id=${wrapAttrValue(id)}></a>`;
+      return this.createAnchor(parent, '', { id, type: 'ref' }).convert();
     });
   });
 };
