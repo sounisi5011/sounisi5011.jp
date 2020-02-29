04629b9384fec9feca1f0f1a06df7675f2fbd5bc
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2020-02-29 14:19:16 +0900

    ES Modules用コードの生成時には@babel/preset-modulesを使用する

--- config/rollup.js
+++ config/rollup.js
@@ -12,7 +12,7 @@ const postcssOptions = {
 };
 const postcssMinify = postcss([cssnano]);
 
-module.exports = ({ outputDir }) => (files, metalsmith) => ({
+module.exports = ({ outputDir }) => (files, metalsmith) => isESModules => ({
   output: {
     dir: outputDir,
     sourcemap: true,
@@ -42,26 +42,28 @@ module.exports = ({ outputDir }) => (files, metalsmith) => ({
       exclude: 'node_modules/**',
       comments: false,
       presets: [
-        [
-          '@babel/preset-env',
-          {
-            corejs: 3,
-            useBuiltIns: 'usage',
-            exclude: [
-              /*
-               * IE11でcore-jsが定義するPromise.all内のiterateが動作しないため除外。
-               * Promiseのpolyfillはmetalsmith-script-module-bundlerが追加するため問題はない。
-               */
-              'es.promise',
-              /*
-               * iterateを使わないのでIE11の問題とは無関係だが、
-               * metalsmith-script-module-bundlerが追加するPromiseのpolyfillがサポート済みのため、
-               * 容量削減の目的で無効化。
-               */
-              'es.promise.finally',
+        isESModules
+          ? ['@babel/preset-modules', {}]
+          : [
+              '@babel/preset-env',
+              {
+                corejs: 3,
+                useBuiltIns: 'usage',
+                exclude: [
+                  /*
+                   * IE11でcore-jsが定義するPromise.all内のiterateが動作しないため除外。
+                   * Promiseのpolyfillはmetalsmith-script-module-bundlerが追加するため問題はない。
+                   */
+                  'es.promise',
+                  /*
+                   * iterateを使わないのでIE11の問題とは無関係だが、
+                   * metalsmith-script-module-bundlerが追加するPromiseのpolyfillがサポート済みのため、
+                   * 容量削減の目的で無効化。
+                   */
+                  'es.promise.finally',
+                ],
+              },
             ],
-          },
-        ],
       ],
     }),
     rollupTerserMinify(),
