8b71df3677e594f22134af1c0821a82744f4b30b
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2020-03-02 13:49:34 +0900

    parse5のNodeを直接操作せず、TreeAdapterの関数を経由する
    TreeAdapterで定義された関数を使用し、parse5のdefault tree adapterに依存しないようにした。

--- plugins/metalsmith/script-module-bundler/index.js
+++ plugins/metalsmith/script-module-bundler/index.js
@@ -10,13 +10,15 @@ const rollup = require('rollup');
 const MIMEType = require('whatwg-mimetype');
 
 const utils = require('./utils');
-const parse5Utils = require('./utils/parse5');
+const parse5UtilsGenerator = require('./utils/parse5');
+
+const parse5Utils = parse5UtilsGenerator({ treeAdapter: htmlparser2Adapter });
 
 /**
  * @typedef {{ path: (function(): string), destination: (function(): string) }} Metalsmith
  * @typedef {Object.<string, MetalsmithFileData>} MetalsmithFiles
  * @typedef {{ contents: Buffer }} MetalsmithFileData
- * @typedef {{ nodeName: string, childNodes?: Parse5Node[] }} Parse5Node
+ * @typedef {{ implementationIsUnknown: true }} Parse5Node
  * @typedef {{ dir: string, sourcemap: boolean | 'inline' | 'hidden' }} RollupOutputOptions
  * @typedef {{ plugins: Object[], output: RollupOutputOptions }} RollupOptions
  * @typedef {{ type: 'chunk', code: string, dynamicImports: string[] }} ChunkInfo
@@ -94,112 +96,109 @@ module.exports = opts => {
       /*
        * HTMLに含まれるscript要素を探す
        */
-      parse5Utils.walk(
-        htmlAST,
-        { treeAdapter: htmlparser2Adapter },
-        targetNode => {
-          if (targetNode.tagName !== 'script') return;
+      parse5Utils.walk(htmlAST, targetNode => {
+        if (!parse5Utils.isElemNode(targetNode, 'script')) return;
 
-          const scriptElemNode = targetNode;
-          const scriptElemAttrs = parse5Utils.getAttrMap(scriptElemNode.attrs);
+        const scriptElemNode = targetNode;
+        const scriptElemAttrs = parse5Utils.getAttrMap(scriptElemNode);
 
-          /**
-           * type属性値の値が以下に当てはまらない場合は処理しない
-           * + 未定義
-           * + 空文字列
-           * + JSのMIMEタイプ
-           * + "module"
-           * @see https://html.spec.whatwg.org/multipage/scripting.html#attr-script-type
-           */
-          const type = scriptElemAttrs.get('type');
-          if (type) {
-            if (
-              type !== 'module' &&
-              !new MIMEType(type).isJavaScript({ allowParameters: true })
-            ) {
-              return;
-            }
-          }
-
-          /**
-           * src属性の値を検証
-           */
-          const src = scriptElemAttrs.get('src');
-          // src属性が存在しない場合は処理しない
-          if (!src) return;
-          // src属性がURLの場合は処理しない
-          if (isUrl(src)) return;
-          // src属性が絶対パスでない場合は処理しない
-          // TODO: 相対パスにも対応する
-          if (!src.startsWith('/')) {
-            throw new Error(
-              `絶対パスではないJSパスは対応していません："${src}" in ${filename}`,
-            );
-          }
-
-          /**
-           * 非同期属性が無いものはスキップする
-           * @see https://html.spec.whatwg.org/multipage/scripting.html#attr-script-async
-           */
+        /**
+         * type属性値の値が以下に当てはまらない場合は処理しない
+         * + 未定義
+         * + 空文字列
+         * + JSのMIMEタイプ
+         * + "module"
+         * @see https://html.spec.whatwg.org/multipage/scripting.html#attr-script-type
+         */
+        const type = scriptElemAttrs.get('type');
+        if (type) {
           if (
             type !== 'module' &&
-            !scriptElemAttrs.has('defer') &&
-            !scriptElemAttrs.has('async')
+            !new MIMEType(type).isJavaScript({ allowParameters: true })
           ) {
-            console.warn(
-              `JSの同期実行はサポートしていません。Rollup.jsの変換対象から除外します："${src}" in ${filename}`,
-            );
             return;
           }
+        }
 
-          /*
-           * src属性値から、JSファイルの絶対パスを導出する
-           */
-          const scriptFileFullpath = path.join(jsRootDirPath, src);
-          scriptFileFullpathSet.add(scriptFileFullpath);
+        /**
+         * src属性の値を検証
+         */
+        const src = scriptElemAttrs.get('src');
+        // src属性が存在しない場合は処理しない
+        if (!src) return;
+        // src属性がURLの場合は処理しない
+        if (isUrl(src)) return;
+        // src属性が絶対パスでない場合は処理しない
+        // TODO: 相対パスにも対応する
+        if (!src.startsWith('/')) {
+          throw new Error(
+            `絶対パスではないJSパスは対応していません："${src}" in ${filename}`,
+          );
+        }
 
-          /*
-           * script要素の更新に必要な情報を追加
-           */
-          const targetFileData = targetFileMap.get(filename);
-          if (!targetFileData) {
-            targetFileMap.set(filename, {
-              filedata,
-              htmlAST,
-              scriptNodeMap: new Map([
+        /**
+         * 非同期属性が無いものはスキップする
+         * @see https://html.spec.whatwg.org/multipage/scripting.html#attr-script-async
+         */
+        if (
+          type !== 'module' &&
+          !scriptElemAttrs.has('defer') &&
+          !scriptElemAttrs.has('async')
+        ) {
+          console.warn(
+            `JSの同期実行はサポートしていません。Rollup.jsの変換対象から除外します："${src}" in ${filename}`,
+          );
+          return;
+        }
+
+        /*
+         * src属性値から、JSファイルの絶対パスを導出する
+         */
+        const scriptFileFullpath = path.join(jsRootDirPath, src);
+        scriptFileFullpathSet.add(scriptFileFullpath);
+
+        /*
+         * script要素の更新に必要な情報を追加
+         */
+        const targetFileData = targetFileMap.get(filename);
+        if (!targetFileData) {
+          targetFileMap.set(filename, {
+            filedata,
+            htmlAST,
+            scriptNodeMap: new Map([
+              [
+                parse5Utils.getParentNode(scriptElemNode),
                 [
-                  scriptElemNode.parentNode,
-                  [
-                    {
-                      node: scriptElemNode,
-                      attrs: scriptElemAttrs,
-                      srcFullpath: scriptFileFullpath,
-                    },
-                  ],
+                  {
+                    node: scriptElemNode,
+                    attrs: scriptElemAttrs,
+                    srcFullpath: scriptFileFullpath,
+                  },
                 ],
-              ]),
+              ],
+            ]),
+          });
+        } else {
+          const { scriptNodeMap } = targetFileData;
+          const scriptParentNode = parse5Utils.getParentNode(scriptElemNode);
+          const scriptNodeList = scriptNodeMap.get(scriptParentNode);
+          if (scriptNodeList) {
+            scriptNodeList.push({
+              node: scriptElemNode,
+              attrs: scriptElemAttrs,
+              srcFullpath: scriptFileFullpath,
             });
           } else {
-            const { scriptNodeMap } = targetFileData;
-            const scriptNodeList = scriptNodeMap.get(scriptElemNode.parentNode);
-            if (scriptNodeList) {
-              scriptNodeList.push({
+            scriptNodeMap.set(scriptParentNode, [
+              {
                 node: scriptElemNode,
                 attrs: scriptElemAttrs,
                 srcFullpath: scriptFileFullpath,
-              });
-            } else {
-              scriptNodeMap.set(scriptElemNode.parentNode, [
-                {
-                  node: scriptElemNode,
-                  attrs: scriptElemAttrs,
-                  srcFullpath: scriptFileFullpath,
-                },
-              ]);
-            }
+              },
+            ]);
           }
-        },
-      );
+        }
+      });
     },
     async after(files, metalsmith) {
       /*
@@ -855,29 +854,23 @@ module.exports = opts => {
             const removedSrcFullpathSet = new Set(
               originalScriptNodeMap.values(),
             );
-            parse5Utils.walk(
-              htmlAST,
-              { treeAdapter: htmlparser2Adapter },
-              targetNode => {
-                if (targetNode.tagName !== 'link') return;
+            parse5Utils.walk(htmlAST, targetNode => {
+              if (!parse5Utils.isElemNode(targetNode, 'link')) return;
 
-                const linkElemNode = targetNode;
-                const linkElemAttrs = parse5Utils.getAttrMap(
-                  linkElemNode.attrs,
-                );
+              const linkElemNode = targetNode;
+              const linkElemAttrs = parse5Utils.getAttrMap(linkElemNode);
 
-                if (linkElemAttrs.get('rel') !== 'preload') return;
-                if (linkElemAttrs.get('as') !== 'script') return;
+              if (linkElemAttrs.get('rel') !== 'preload') return;
+              if (linkElemAttrs.get('as') !== 'script') return;
 
-                const href = linkElemAttrs.get('href');
-                if (!href || !href.startsWith('/')) return;
-                const preloadFileFullpath = path.join(jsRootDirPath, href);
+              const href = linkElemAttrs.get('href');
+              if (!href || !href.startsWith('/')) return;
+              const preloadFileFullpath = path.join(jsRootDirPath, href);
 
-                if (removedSrcFullpathSet.has(preloadFileFullpath)) {
-                  parse5Utils.detachNode(targetNode);
-                }
-              },
-            );
+              if (removedSrcFullpathSet.has(preloadFileFullpath)) {
+                parse5Utils.detachNode(linkElemNode);
+              }
+            });
           }
         }
 
--- plugins/metalsmith/script-module-bundler/utils/parse5.js
+++ plugins/metalsmith/script-module-bundler/utils/parse5.js
@@ -1,109 +1,154 @@
-const htmlparser2Adapter = require('parse5-htmlparser2-tree-adapter');
-
-function walk(node, { treeAdapter }, callback) {
-  if (callback(node) === false) {
-    return false;
-  }
-  for (const childNode of treeAdapter.getChildNodes(node)) {
-    if (callback(childNode) === false) {
+module.exports = ({ treeAdapter }) => {
+  function walk(node, callback) {
+    if (callback(node) === false) {
       return false;
     }
-  }
-}
-exports.walk = walk;
-
-/**
- * @param {(Object.<string, string|null>|Map.<string, string|null>|Set.<string>)[]} attrsList
- * @returns {{name: string, value: string}[]}
- * @see https://github.com/inikulin/parse5/blob/master/packages/parse5/docs/tree-adapter/default/attribute.md
- */
-function createElemAttrs(...attrsList) {
-  /** @type {Map.<string, string>} */
-  const attrsMap = new Map();
-  for (const attrs of attrsList) {
-    if (attrs instanceof Set) {
-      for (const name of attrs) {
-        attrsMap.set(name, '');
+    for (const childNode of treeAdapter.getChildNodes(node) || []) {
+      if (walk(childNode, callback) === false) {
+        return false;
       }
-    } else {
-      for (const [name, value] of attrs instanceof Map
-        ? attrs
-        : Object.entries(attrs)) {
-        if (typeof value === 'string' || value) {
-          attrsMap.set(name, value);
-        } else {
-          attrsMap.delete(name);
+    }
+  }
+
+  /**
+   * @param {*} node
+   * @param {*} tagName
+   */
+  function isElemNode(node, tagName = null) {
+    if (!treeAdapter.isElementNode(node)) {
+      return false;
+    }
+    if (!tagName) {
+      return true;
+    }
+    return treeAdapter.getTagName(node) === tagName;
+  }
+
+  /**
+   * @param {(Object.<string, string|null>|Map.<string, string|null>|Set.<string>)[]} attrsList
+   * @returns {{name: string, value: string}[]}
+   * @see https://github.com/inikulin/parse5/blob/master/packages/parse5/docs/tree-adapter/default/attribute.md
+   */
+  function createElemAttrs(...attrsList) {
+    /** @type {Map.<string, string>} */
+    const attrsMap = new Map();
+    for (const attrs of attrsList) {
+      if (attrs instanceof Set) {
+        for (const name of attrs) {
+          attrsMap.set(name, '');
+        }
+      } else {
+        for (const [name, value] of attrs instanceof Map
+          ? attrs
+          : Object.entries(attrs)) {
+          if (typeof value === 'string' || value) {
+            attrsMap.set(name, value);
+          } else {
+            attrsMap.delete(name);
+          }
         }
       }
     }
+    return [...attrsMap].map(([name, value]) => ({ name, value }));
   }
-  return [...attrsMap].map(([name, value]) => ({ name, value }));
-}
 
-function isNode(value) {
-  return (
-    value && typeof value === 'object' && typeof value.nodeName === 'string'
-  );
-}
+  function isNode(value) {
+    return (
+      value && typeof value === 'object' && treeAdapter.isElementNode(value)
+    );
+  }
 
-/**
- * @param {string} tagName
- * @param {Object.<string, string>|Map.<string, string>|Set.<string>|(Object.<string, string>|Map.<string, string>|Set.<string>)[]} attrs
- * @param {string|Object|(string|Object)[]} content
- * @param {string} namespaceURI
- */
-exports.createElement = (
-  tagName,
-  attrs,
-  content = null,
-  namespaceURI = 'http://www.w3.org/1999/xhtml',
-) => {
-  const elem = htmlparser2Adapter.createElement(
+  /**
+   * @param {string} tagName
+   * @param {Object.<string, string>|Map.<string, string>|Set.<string>|(Object.<string, string>|Map.<string, string>|Set.<string>)[]} attrs
+   * @param {string|Object|(string|Object)[]} content
+   * @param {string} namespaceURI
+   */
+  function createElement(
     tagName,
-    namespaceURI,
-    Array.isArray(attrs) ? createElemAttrs(...attrs) : createElemAttrs(attrs),
-  );
-  const contentList = Array.isArray(content) ? content : [content];
-  for (const content of contentList) {
-    if (content) {
-      if (isNode(content)) {
-        htmlparser2Adapter.appendChild(elem, content);
-      } else {
-        htmlparser2Adapter.insertText(elem, String(content));
+    attrs,
+    content = null,
+    namespaceURI = 'http://www.w3.org/1999/xhtml',
+  ) {
+    const elem = treeAdapter.createElement(
+      tagName,
+      namespaceURI,
+      Array.isArray(attrs) ? createElemAttrs(...attrs) : createElemAttrs(attrs),
+    );
+    const contentList = Array.isArray(content) ? content : [content];
+    for (const content of contentList) {
+      if (content) {
+        if (isNode(content)) {
+          treeAdapter.appendChild(elem, content);
+        } else {
+          treeAdapter.insertText(elem, String(content));
+        }
       }
     }
+    return elem;
   }
-  return elem;
-};
 
-/**
- * @param {Object} node
- * @returns {string}
- */
-exports.getNodePath = node => {
-  const pathList = [];
-  while (node) {
-    const { parentNode } = node;
-    let nodeName = node.nodeName;
-    if (parentNode) {
-      nodeName += `[${parentNode.childNodes.indexOf(node)}]`;
+  /**
+   * @param {Object} node
+   * @returns {string}
+   */
+  function getNodePath(node) {
+    const pathList = [];
+    while (node) {
+      const parentNode = treeAdapter.getParentNode(node);
+
+      /**
+       * @see https://github.com/inikulin/parse5/blob/master/packages/parse5/docs/tree-adapter/default/interface-list.md
+       */
+      let nodeName = node.nodeName;
+      if (!nodeName) {
+        if (treeAdapter.isElementNode(node)) {
+          nodeName = treeAdapter.getTagName(node);
+        } else if (treeAdapter.isTextNode(node)) {
+          nodeName = '#text';
+        } else if (treeAdapter.isCommentNode(node)) {
+          nodeName = '#comment';
+        } else if (treeAdapter.isDocumentTypeNode(node)) {
+          nodeName = '#documentType';
+        } else {
+          /**
+           * @see https://github.com/inikulin/parse5/blob/master/packages/parse5-htmlparser2-tree-adapter/docs/document.md
+           * @see https://github.com/inikulin/parse5/blob/master/packages/parse5-htmlparser2-tree-adapter/docs/document-fragment.md
+           */
+          nodeName = `#${node.nodeType}`;
+        }
+      }
+
+      if (parentNode) {
+        const childNodeIndex = treeAdapter
+          .getChildNodes(parentNode)
+          .indexOf(node);
+        nodeName += `[${childNodeIndex}]`;
+      }
+      pathList.unshift(nodeName);
+      node = parentNode;
     }
-    pathList.unshift(nodeName);
-    node = parentNode;
+    return pathList.join('/');
   }
-  return pathList.join('/');
-};
 
-exports.getAttrMap = attrs => {
-  /** @type {Map.<string, string>} */
-  const map = new Map();
-  for (const attr of attrs) {
-    if (attr.namespace || attr.prefix || map.has(attr.name)) continue;
-    map.set(attr.name, attr.value);
+  function getAttrMap(node) {
+    /** @type {Map.<string, string>} */
+    const map = new Map();
+    for (const attr of treeAdapter.getAttrList(node)) {
+      if (attr.namespace || attr.prefix || map.has(attr.name)) continue;
+      map.set(attr.name, attr.value);
+    }
+    return map;
   }
-  return map;
+
+  return {
+    walk,
+    isElemNode,
+    createElement,
+    getNodePath,
+    getAttrMap,
+    getParentNode: treeAdapter.getParentNode,
+    appendChild: treeAdapter.appendChild,
+    detachNode: treeAdapter.detachNode,
+  };
 };
-
-exports.appendChild = htmlparser2Adapter.appendChild;
-
-exports.detachNode = htmlparser2Adapter.detachNode;
