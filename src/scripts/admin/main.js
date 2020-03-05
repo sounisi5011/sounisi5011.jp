93a99c35e48bc415dc2b71f70eae2b5ba50bdf4e
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2020-03-06 00:29:03 +0900

    仮設エディタのプレビュー画面で本番サイトと同じCSSを使用

--- src/scripts/admin/main.js
+++ src/scripts/admin/main.js
@@ -16,13 +16,24 @@ const editorElem = h('textarea', {
 
 const previewElem = h('iframe', { className: 'preview', style: { flex: 1 } });
 
+const novelBodyElem = h('main', { className: 'novel-body' });
+
 function updatePreview(inputText) {
   const html = asciidoctor.convert(inputText);
-  previewElem.contentDocument.body.innerHTML = html;
+  novelBodyElem.innerHTML = html;
 }
 
 document.body.appendChild(editorElem);
+
 document.body.appendChild(previewElem);
+(previewDoc => {
+  previewDoc.documentElement.lang = 'ja';
+  for (const href of ['/default.css', '/novels.css']) {
+    previewDoc.head.appendChild(h('link', { rel: 'stylesheet', href }));
+  }
+  previewDoc.body.appendChild(novelBodyElem);
+})(previewElem.contentDocument);
+
 setAttr(document.body, {
   style: {
     display: 'flex',
